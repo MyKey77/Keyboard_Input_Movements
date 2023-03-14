c8ddf08e8496d18874030d85590f6b4a55399e5d
Mikey <mikey.mazza1999@gmail.com>
2023-03-14 17:44:31 +0100

    complet

--- src/main/resources/static/movements/app.js
+++ src/main/resources/static/movements/app.js
@@ -2,21 +2,21 @@ const image = document.getElementById("character");
 let x = 0;
 let y = 0;
 image.style.position = "absolute";
-image.style.left = x + "px";
-image.style.top = y + "px";
+image.style.left = x + "px";  //X kordínáta
+image.style.top = y + "px";   //y kodínáta mozgás
 
-document.addEventListener("keydown", (event) => {
+document.addEventListener("keydown", (event) => { //Billentyűzet hallgoató
     switch (event.key) {
-        case "ArrowUp":
+        case "ArrowUp":     //FEL GOMB
             y -= 10;
             break;
-        case "ArrowDown":
+        case "ArrowDown":    //LE GOMB
             y += 10;
             break;
-        case "ArrowLeft":
+        case "ArrowLeft":    //BAL GOMB
             x -= 10;
             break;
-        case "ArrowRight":
+        case "ArrowRight":    //JOBB GOMB
             x += 10;
             break;
         default:
