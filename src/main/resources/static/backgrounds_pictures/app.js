81f4dc9c6a6b4a23a91a81060711e89a0e824458
Mikey <mikey.mazza1999@gmail.com>
2023-03-14 17:13:20 +0100

    change

--- src/main/resources/static/backgrounds_pictures/app.js
+++ src/main/resources/static/backgrounds_pictures/app.js
@@ -1,34 +1,28 @@
-let gameDiv=document.getElementById('game');
-let charDiv=document.getElementById('character');
+const image = document.getElementById("character");
+let x = 0;
+let y = 0;
+image.style.position = "absolute";
+image.style.left = x + "px";
+image.style.top = y + "px";
 
-document.addEventListener('keydown', handleKeys)
-var leftright=0;
-var updown=0;
-
-
-function handleKeys(e){
-    let keypress=e.code;   //BAL GOMB
-    if(keypress === "ArrowRight"){
-        console.log('right arrow pressed');
-        leftright +=10;
-        charDiv.style.left=leftright+'px';}
-
-    if(keypress === 'ArrowLeft'){ // JOBB GOMB
-        console.log('left arrow pressed')
-        leftright -=10;
-
-        charDiv.style.left=leftright+'px';}
-
-
-    if(keypress === 'ArrowUp'){ // FEL GOMB
-        console.log('up arrow pressed')
-        updown -=10;
-        charDiv.style.top =updown+'px';
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            y -= 10;
+            break;
+        case "ArrowDown":
+            y += 10;
+            break;
+        case "ArrowLeft":
+            x -= 10;
+            break;
+        case "ArrowRight":
+            x += 10;
+            break;
+        default:
+            return;
     }
 
-    if(keypress === 'ArrowDown'){ // LE GOMB
-        console.log('down arrow pressed')
-        updown +=10;
-        charDiv.style.top =updown+'px';
-    }
-}
+    image.style.left = x + "px";
+    image.style.top = y + "px";
+});
